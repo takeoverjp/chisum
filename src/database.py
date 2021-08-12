804b0e6e727955767a1024a69ca1facb958f5a5f
Kondo Takeo <takeoverjp@gmail.com>
2021-08-12 21:49:01 +0900

    feat: create table
    Change-Id: I174d5bc3979dd473830de73136c0731b5c297689

--- src/database.py
+++ src/database.py
@@ -2,9 +2,29 @@ import sqlite3
 
 
 class Database:
-    def __init__(self, file_name):
+    def __init__(self, file_name, table_name):
         self._file_name = file_name
+        self._table_name = table_name
         self._connection = sqlite3.connect(self._file_name)
+        self._cursor = self._connection.cursor()
+        if not self.has_table():
+            self._cursor.execute(
+                f'CREATE TABLE {self._table_name}'
+                '  (timestamp TEXT, path TEXT, count INTEGER)'
+            )
+            self._connection.commit()
 
     def __del__(self):
         self._connection.close()
+
+    def has_table(self):
+        return self._cursor.execute(
+            'SELECT COUNT(*) from sqlite_master'
+            f'  WHERE TYPE=\'table\''
+            f'    AND name=\'{self._table_name}\'')\
+            .fetchone()[0] == 1
+
+    def dump_master(self):
+        print(self._cursor.execute(
+            'SELECT * from sqlite_master')
+            .fetchall())
--- test/test_database.py
+++ test/test_database.py
@@ -10,6 +10,7 @@ class TestDataBase(unittest.TestCase):
     def setUp(self) -> None:
         if os.path.isfile(self.test_db_name):
             os.unlink(self.test_db_name)
+        self.assertFalse(os.path.isfile(self.test_db_name))
         return super().setUp()
 
     def tearDown(self) -> None:
@@ -18,9 +19,24 @@ class TestDataBase(unittest.TestCase):
         return super().tearDown()
 
     def test_create_database(self):
-        self.assertFalse(os.path.isfile(self.test_db_name))
-        Database(self.test_db_name)
+        # Execute
+        db = Database(self.test_db_name, "items")
+
+        # Assert
         self.assertTrue(os.path.isfile(self.test_db_name))
+        self.assertTrue(db.has_table())
+
+    def test_connect_exist_database(self):
+        # SetUp
+        db = Database(self.test_db_name, "items")
+        self.assertTrue(os.path.isfile(self.test_db_name))
+
+        # Execute
+        db = Database(self.test_db_name, "items")
+
+        # Assert
+        self.assertTrue(os.path.isfile(self.test_db_name))
+        self.assertTrue(db.has_table())
 
 
 if __name__ == '__main__':
