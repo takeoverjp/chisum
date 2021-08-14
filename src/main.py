7b587017f9526fd671207b613d5d4eeb4345c990
Kondo Takeo <takeoverjp@gmail.com>
2021-08-14 13:36:00 +0900

    feat: support record subcommand
    Change-Id: I7025356965e361d3b9755ea9730ff1510461bf41

--- src/main.py
+++ src/main.py
@@ -1,16 +1,29 @@
 from datetime import datetime
 
+import click
+
 from src.count_controller import CountController
 from src.database_count_repository import DatabaseCountRepository
 from src.record_count_use_case_interactor import RecordCountUseCaseInteractor
 
+@click.group()
+def cmd():
+    pass
 
-def main():
+@cmd.command()
+@click.argument('source', type=str)
+@click.argument('dest_database', type=str)
+@click.argument('table', type=str)
+def record(source, dest_database, table):
     repository = DatabaseCountRepository(
-        "chisum.db", "table1")
+        dest_database, table)
     interactor = RecordCountUseCaseInteractor(repository)
     controller = CountController(interactor)
-    controller.record("testdata/libs.txt", datetime.now())
+    controller.record(source, datetime.now())
+
+
+def main():
+    cmd()
 
 
 if __name__ == '__main__':
